6894419611fff823e2757657f8c5088609fe45df
Andrew Ghostuhin <me@torinasakura.name>
2020-07-30 11:16:27 +0500

    feat: query options
    affects: @microfleet/clickhouse-adapter

--- packages/clickhouse-adapter/test/suites/04-insert-data.test.ts
+++ packages/clickhouse-adapter/test/suites/04-insert-data.test.ts
@@ -62,4 +62,64 @@ describe('Clickhouse Adapter', () => {
       )
     })
   })
+
+  it('insert tsv', async (done: jest.DoneCallback) => {
+    const migrator = new Migrator(ch)
+
+    migrator.addMigration({
+      name: '1_event_b',
+      async up(clickhouseClient: ClickhouseClient): Promise<boolean> {
+        await clickhouseClient.createTable(
+          new TableMaker(DB_NAME, 'event_b', null, {
+            columnDefinitions: [
+              { name: 'trackDate', type: 'Date' },
+              { name: 'trackTimestamp', type: 'DateTime' },
+              { name: 'eventType', type: 'String' },
+              { name: 'userId', type: 'String' },
+              { name: 'code', type: 'Int32' },
+            ],
+            tableOptions: ['ENGINE = MergeTree(trackDate, (trackTimestamp, eventType), 8192)'],
+          })
+        )
+        return true
+      },
+    })
+
+    await migrator.up(migrator.migrateAll(DB_NAME))
+
+    const now = moment()
+    const data = [
+      {
+        trackDate: moment(now).format('YYYY-MM-DD'),
+        trackTimestamp: moment(now).format('YYYY-MM-DD HH:mm:ss'),
+        eventType: 'type_a',
+      },
+    ]
+    const fields = ['trackDate', 'trackTimestamp', 'eventType', 'userId', 'code']
+    const getArray = (data: any): any => {
+      return fields.map((key: string) => data[key])
+    }
+
+    const insertData: InsertData = {
+      query: () => {
+        return `INSERT INTO event_b (${fields.join(',')})`
+      },
+      data: () => {
+        return data.map(getArray)
+      },
+    }
+
+    ch.insert(DB_NAME, insertData, { format: 'TabSeparated' }, () => {
+      ch.connection.query(
+        'SELECT * FROM event_b',
+        { syncParser: true, queryOptions: { database: DB_NAME } },
+        (_: any, result: any) => {
+          assert(result)
+          assert(result.data)
+          assert(result.data.length)
+          done()
+        }
+      )
+    })
+  })
 })
