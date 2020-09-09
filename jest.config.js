5ad6e99736693f867219a175f8cdb78490ae40e3
pajgo <pavel.rogovoi@gmail.com>
2020-09-09 22:06:31 +0600

    chore(general): enable test coverage + run tests in one shot

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['packages/**/src/**/*.{ts,tsx,js,jsx}', 'packages/**/!src/**/*.d.ts'],
+  coverageReporters: ['text-summary', 'json', 'lcov'],
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.json',
